2b99fa4873483c5767da3453ae0398f1edb2ea26
Nedeljko Šovljanski <sovljaned@gmail.com>
2022-01-07 21:43:50 +0100

    Welcoming screen

---
+++ EightQueens/Program.cs
@@ -0,0 +1,2 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("This application will find all possible solutions how to put eight queens on chess board without attacking each other");
