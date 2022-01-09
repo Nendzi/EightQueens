737d2dad080d28b7ddb367b61a98a9b437f3f58f
Nedeljko Šovljanski <sovljaned@gmail.com>
2022-01-09 22:32:13 +0100

    Final version

--- EightQueensSol/EightQueens/Program.cs
+++ EightQueensSol/EightQueens/Program.cs
@@ -6,13 +6,30 @@ namespace EightQueens
     {
         static void Main()
         {
-            Console.WriteLine("Welcome. Application wiil find all solutions for you.");
+            Console.WriteLine("Welcome. Application will find all solutions for you.");
 
             ProgramLogic programLogic = new ProgramLogic(8);
 
             Matrix perm = programLogic.FindAllPermutations();
             List<string> solutions = programLogic.SearchForSolutions(perm);
 
+            int count = 1;
+            foreach (var solution in solutions)
+            {
+                if (count < 10)
+                {
+                    Console.Write(count + ".  ");
+                }
+                else
+                {
+                    Console.Write(count + ". ");
+                }
+                programLogic.EncodeSolution(solution);
+                programLogic.VisiulizeSolution(solution);
+                Console.WriteLine();
+                Console.WriteLine("____________________________________");
+                count++;
+            }
             Console.ReadLine();
         }
     }
@@ -34,7 +51,6 @@ namespace EightQueens
             output = PermutationsCalculator.Permutations(_numberOfQueens);
             return output;
         }
-
         public List<string> SearchForSolutions(Matrix perm)
         {
             List<string> output = new List<string>();
@@ -58,17 +74,14 @@ namespace EightQueens
                 {
                     for (int x = 0; x < _numberOfQueens; x++)
                     {
-                        Console.Write(perm.item[i, x]);
                         oneRow += perm.item[i, x].ToString();
                     }
-                    Console.Write("\t");
                     output.Add(oneRow);
                     oneRow = "";
                 }
             }
             return output;
         }
-
         private void StrikeThroughFields(int row, int column)
         {
             int zeroBasedNumberOfElements = _numberOfQueens - 1;
@@ -104,7 +117,6 @@ namespace EightQueens
             }
             _chessBoard.item[row, column]++;
         }
-
         private bool FieldIsEmpty(int row, int column)
         {
             if (_chessBoard.item[row, column] != 0)
@@ -113,8 +125,7 @@ namespace EightQueens
             }
             return true;
         }
-
-        void ClearTable()
+        private void ClearTable()
         {
             for (int i = 0; i < _numberOfQueens; i++)
             {
@@ -123,6 +134,113 @@ namespace EightQueens
                     _chessBoard.item[i, j] = 0;
                 }
             }
+        }
+        internal void EncodeSolution(string solution)
+        {
+            string output = "";
+
+            for (int i = 0; i < solution.Length; i++)
+            {
+                output += Convert.ToChar(65 + i).ToString() + (Convert.ToInt32(solution[i].ToString()) + 1).ToString() + " ";
+            }
+
+            Console.WriteLine(output);
+        }
+        internal void VisiulizeSolution(string solution)
+        {
+            //top border
+            string row1 = "";
+            row1 += Convert.ToChar(0x250C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x252C);
+            row1 += Convert.ToChar(0x2500);
+            row1 += Convert.ToChar(0x2510);
+
+            // middle border
+            string row3 = "";
+            row3 += Convert.ToChar(0x251C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x253C);
+            row3 += Convert.ToChar(0x2500);
+            row3 += Convert.ToChar(0x2524);
+
+            // bottom border
+            string row4 = "";
+            row4 += Convert.ToChar(0x2514);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2534);
+            row4 += Convert.ToChar(0x2500);
+            row4 += Convert.ToChar(0x2518);
+
+            Console.WriteLine(row1);
+            for (int i = 7; i >= 0; i--)
+            {
+                FormRowOnChessBoard(i, solution);
+                if (i == 0)
+                {
+                    Console.WriteLine(row4);
+                }
+                else
+                {
+                    Console.WriteLine(row3);
+                }
+            }
+        }
+        private string PutQueen(int location, int position, string solution)
+        {
+            int extract = Convert.ToInt32(solution[location].ToString());
+            if (position == extract)
+            {
+                return Convert.ToChar(0x263A).ToString();
+            }
+            return " ";
+        }
+        private void FormRowOnChessBoard(int position, string solution)
+        {
+            string output = "";
+            output += Convert.ToChar(0x2502);
+            for (int i = 0; i < _numberOfQueens; i++)
+            {
+                output += PutQueen(i, position, solution);
+                output += Convert.ToChar(0x2502);
+            }
+            Console.WriteLine(output);
         }
     }
 }
